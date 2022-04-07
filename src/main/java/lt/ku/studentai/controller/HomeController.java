22e4e76a2220d66f4ee46fde236e45d9c8ce18d6
GediminasGricius <g.gricius@ituostas.lt>
2022-04-07 11:17:59 +0300

    Update group

--- src/main/java/lt/ku/studentai/controller/HomeController.java
+++ src/main/java/lt/ku/studentai/controller/HomeController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lt.ku.studentai.entities.Group;
 import lt.ku.studentai.repositories.GroupRepository;
@@ -19,9 +22,31 @@ public class HomeController {
 	@GetMapping("/")
 	public String home(Model model) {
 		model.addAttribute("groups", groupService.getGroups());
-		
-		
 		return "home";
+	}
+	
+	@GetMapping("/groupNew")
+	public String groupNew(Model model) {
+		return "group_new";
+	}
+	
+	@PostMapping("/groupNew")
+	public String addGroup(@RequestParam("name") String name, @RequestParam("year") Integer year) {
+		Group g=new Group(name,year);
+		groupService.addGroup(g);
+		return "redirect:/";
+	}
+	
+	@GetMapping("/groupUpdate")
+	public String groupNew(@RequestParam("id") Integer id, Model model) {
+		model.addAttribute("group", groupService.getGroup(id));
+		return "group_update";
+	}
+	
+	@PostMapping("/groupUpdate")
+	public String groupUpdate(@ModelAttribute Group g) {
+		groupService.updateGroup(g);
+		return "redirect:/";
 	}
 
 }
